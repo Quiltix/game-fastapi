bb304a4b1906b5beb9047d072aa816b2a6fca793
Quiltix <vanya200632@gmail.com>
2025-11-28 20:38:20 +0300

    refactor(base): delete useless methods

--- src/db/models/base.py
+++ src/db/models/base.py
@@ -11,19 +11,3 @@ class BaseModel(DeclarativeBase):
     """Base model for SQLAlchemy ORM."""
 
     __abstract__ = True
-
-    def __repr__(self) -> str:
-        """
-        Возвращает удобное для отладки представление объекта.
-        Пример: <User id=1>
-        """
-        pk_name = self.__table__.primary_key.columns.keys()[0]
-        pk_value = getattr(self, pk_name, None)
-        return f"<{self.__class__.__name__} {pk_name}={pk_value}>"
-
-    def to_dict(self) -> dict[str, Any]:
-        """
-        Преобразует объект модели в словарь.
-        Безопасный способ, который включает только колонки таблицы.
-        """
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
