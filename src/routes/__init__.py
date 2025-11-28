859444a299761a8be6d6de5f274022cdbad6afb1
Quiltix <vanya200632@gmail.com>
2025-11-28 20:27:14 +0300

    fix(router): final clear of websockets

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,7 +2,7 @@
 
 from .auth import router as auth_router
 from .user import router as user_router
+from .games import router as games_router
 
 
-
-routers = [auth_router,user_router]
+routers = [auth_router,user_router,games_router]
