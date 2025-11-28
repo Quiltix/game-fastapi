af891874bfb3c9230afdbd79af145033c815d7bb
Quiltix <vanya200632@gmail.com>
2025-11-28 22:11:11 +0300

    feat(user): routers for update username and password

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status
 
 from src.core.depends import DatabaseDep, UserDep
+from src.schemas.auth import UserUpdateUsernameSchema, UserUpdatePasswordSchema
 from src.schemas.game import GameResponseSchema
 from src.schemas.user import UserResponseSchema
 from src.services import game as game_service
@@ -25,3 +26,39 @@ async def get_my_game_history(user_id: UserDep, db: DatabaseDep):
 
     Необходима авторизация."""
     return await game_service.get_user_game_history(db=db, user_id=user_id)
+
+@router.patch("/me/username", response_model=UserResponseSchema, summary="Изменить имя пользователя")
+async def update_my_username(schema: UserUpdateUsernameSchema, user_id: UserDep, db: DatabaseDep):
+    """
+    Обновляет имя текущего аутентифицированного пользователя.
+
+    Имя пользователя должно быть уникальным и не текущим.
+    """
+    updated_user = await user_service.update_username(
+        db=db,
+        user_id=user_id,
+        new_username=schema.new_username
+    )
+    return updated_user
+
+
+@router.patch("/me/password", status_code=status.HTTP_200_OK, summary="Изменить пароль")
+
+async def update_my_password(
+    schema: UserUpdatePasswordSchema,
+    user_id: UserDep,
+    db: DatabaseDep
+):
+    """
+    Обновляет пароль текущего аутентифицированного пользователя.
+
+    Требуется подтверждение текущего пароля.
+
+    Пароль не может совпадать с текущим.
+    """
+    await user_service.update_password(
+        db=db,
+        user_id=user_id,
+        current_password=schema.current_password,
+        new_password=schema.new_password
+    )
